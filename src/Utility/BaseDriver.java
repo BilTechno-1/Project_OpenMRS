37957c4b028a8726f900ae8bfaefcc4cbaf801fa
leyla <leylabilcan31@gmail.com>
2024-10-06 22:16:01 +0300

    Initial Proje Oluşturuldu

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,37 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseDriver {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void InitialTransactions(){
+       // System.out.println("Başlangıç işlemleri yapılıyor");
+
+        driver=new ChromeDriver();
+
+        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+    }
+
+    @AfterClass
+    public void ClosingTransactions(){
+       // System.out.println("Kapanis işlemleri yapılıyor");
+
+        Tools.Wait(3);
+        driver.quit();
+    }
+
+
+}
---
+++ src/Utility/Tools.java
@@ -0,0 +1,19 @@
+package Utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class Tools {
+
+    public static void Wait(int sn) {
+
+        try {
+            Thread.sleep(sn * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
